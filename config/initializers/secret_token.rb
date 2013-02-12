c638e19579cea277b0707eeaaafd543b741cdcbf
openpartners <andrzejsew@gmail.com>
2013-02-12 15:44:09 +0100

    first commit opcut

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opcut::Application.config.secret_token = '01cc70b85c35ca193ae55103ae3e79fdeb87d2a39ba830f1cc2b3ba912422f7a2d461785c7bb63d510f2471cb5c4535aa75c615642b8160d7f16a3cfacad5c7a'
